b0646a958f298123d4a53b4fa245092a1c996476
Amna Janahi <amna.m.janahi@gmail.com>
2025-09-03 21:37:13 +0300

    adding login to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { jwtDecode } from "jwt-decode";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link  } from "react-router";
 
 import Homepage from "./components/Homepage/Homepage"
 import RegisterForm from "./components/RegisterForm/RegisterForm"
+import LoginForm from "./components/LoginForm/LoginForm";
 import RideForm from "./components/RideForm/RideForm";
 import HistoryRideList from "./components/HistoryRideList/HistoryRideList"
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
   const [formIsShown, setFormIsShown] = useState(false);
+  
+  function handleLogin(newToken) {
+    setToken(newToken);
+  } 
+  
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    console.log(decodedToken);
+  }
+
   return (
   <div>
 
@@ -15,12 +28,15 @@ const App = () => {
       <nav>
         {/* <Link to="/">Home</Link> */}
         <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Log in</Link>
         <Link to="/rides/request">Request Ride</Link>
         <Link to="/rides/myrides">My Rides</Link>
       </nav>
       <Routes>
-       <Route path="/signup" element={<RegisterForm/>}/>
-        <Route
+      <Route path="/signup" element={<RegisterForm/>}/>
+      <Route path="/login" element={<LoginForm onLogin={handleLogin}/>}/>
+      
+      <Route
           path="/rides/request"
           element={
           <RideForm
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router'
 import axios from 'axios'
-import "./AuthForm.css"
+
 
 function LoginForm({ onLogin }) {
   const [name, setName] = useState('')
